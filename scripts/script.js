bf5a232330de9bb21aecfebb4f6870b4f70c39c8
alvad89 <alvad89@gmail.com>
2013-02-05 17:45:50 +0600

    Add table to main page in 2.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,55 @@
-var d = document.createElement('div');
-d.className='div_one';
-document.body.appendChild(d);
+
+
+//buttonTable
+/*$(document).ready(function (){
+   var $as = $('div a') 
+   var $ims = $('div p');
+    $as.click(function(){
+      $ims.hide();  
+      $ims.eq($as.index(this)).show();  
+    }); 
+     
+});*/
+var tableHolder = $('#tableHolder');
+var holder = $('#holder');
+
+function isTable(){
+tableHolder = $('#tableHolder');
+var buttonTable = $('#tableButton');
+buttonTable.click(function(){
+holder.hide();
+console.log(holder)
+tableHolder.show();
+
+});}
+
+//buttonGraph
+function isGraph(){
+holder = $('#holder');
+var buttonGraph = $('#graphButton');
+buttonGraph.click(function(){
+tableHolder.hide();
+console.log(holder)
+holder.show();
+});}
+
+
+$(document).ready( function () {
+	$('#example').dataTable( {
+//T<"clear">lfrtip   <"H"Tfr>t<"F"ip>
+		"bJQueryUI": true,
+		"sPaginationType": "full_numbers",
+		"sDom": 'T<"clear">lfrtip<"H">t',
+		"oTableTools": {
+			"aButtons": [
+				"copy", "csv", "xls", "pdf",
+				{
+					"sExtends":    "collection",
+					"sButtonText": "Save",
+					"aButtons":    [ "csv", "xls", "pdf" ]
+				}
+			]
+		}
+	} );
+} );
 
